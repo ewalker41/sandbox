552c56141c3b1570db0b27a66a8627799b891093
Eric Walker <ewalker@factual.com>
2015-07-29 11:08:53 -0700

    First update to output string

--- mysheet.js
+++ mysheet.js
@@ -1,11 +1,10 @@
-
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu('CodeSchool')
-      .addItem('Hello World!', 'showDialog')
+      .addItem('Hello World! Example', 'showDialog')
       .addToUi();
 }
  
 function showDialog() {
-  SpreadsheetApp.getUi().alert('Hello World!');
+  SpreadsheetApp.getUi().alert('Hello, I\'m Factual!');
 }
