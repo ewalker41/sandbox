1ed23b3fb09619470b23633841975e839280b369
Eric Walker <ewalker@factual.com>
2015-07-29 00:48:34 -0700

    Create mysheet.js

---
+++ mysheet.js
@@ -0,0 +1,11 @@
+
+function onOpen() {
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu('CodeSchool')
+      .addItem('Hello World!', 'showDialog')
+      .addToUi();
+}
+ 
+function showDialog() {
+  SpreadsheetApp.getUi().alert('Hello World!');
+}
